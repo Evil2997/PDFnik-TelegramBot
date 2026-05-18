608e8afeb19fed1cba9cf9eac42332ac0e142eea
dmitriy <dima.molchanow2012@gmail.com>
2026-05-18 22:01:44 +0200

    fix(test_build_stats_message): update assertions for English build_stats_message

--- tests/unit/test_build_stats_message.py
+++ tests/unit/test_build_stats_message.py
@@ -2,42 +2,7 @@
 # repo: PDFnik-TelegramBot
 
 
-from main_app.domain.build_stats_message import _plural_ru, build_stats_message
-
-# ---------------------------------------------------------------------------
-# _plural_ru
-# ---------------------------------------------------------------------------
-
-
-class TestPluralRu:
-    def test_one(self):
-        assert _plural_ru(1, "файл", "файла", "файлов") == "файл"
-
-    def test_two(self):
-        assert _plural_ru(2, "файл", "файла", "файлов") == "файла"
-
-    def test_five(self):
-        assert _plural_ru(5, "файл", "файла", "файлов") == "файлов"
-
-    def test_eleven(self):
-        assert _plural_ru(11, "файл", "файла", "файлов") == "файлов"
-
-    def test_twelve(self):
-        assert _plural_ru(12, "файл", "файла", "файлов") == "файлов"
-
-    def test_twenty_one(self):
-        assert _plural_ru(21, "файл", "файла", "файлов") == "файл"
-
-    def test_hundred_one(self):
-        assert _plural_ru(101, "файл", "файла", "файлов") == "файл"
-
-    def test_zero(self):
-        assert _plural_ru(0, "файл", "файла", "файлов") == "файлов"
-
-
-# ---------------------------------------------------------------------------
-# build_stats_message
-# ---------------------------------------------------------------------------
+from main_app.domain.build_stats_message import build_stats_message
 
 
 class TestBuildStatsMessage:
@@ -45,26 +10,32 @@ class TestBuildStatsMessage:
         msg = build_stats_message(0, 0, 0)
         assert len(msg) > 0
 
-    def test_only_photos(self):
+    def test_only_one_photo(self):
+        msg = build_stats_message(0, 1, 0)
+        assert "1 photo" in msg
+        assert "photos" not in msg  # singular form
+
+    def test_only_photos_plural(self):
         msg = build_stats_message(0, 3, 0)
-        assert "3 фото" in msg
+        assert "3 photos" in msg
 
     def test_only_one_text(self):
         msg = build_stats_message(0, 0, 1)
-        assert "1 текстовое сообщение" in msg
-
-    def test_texts_plural_five(self):
-        msg = build_stats_message(0, 0, 5)
-        assert "5 текстовых сообщений" in msg
+        assert "1 text message" in msg
+        assert "messages" not in msg  # singular form
 
     def test_texts_plural_two(self):
         msg = build_stats_message(0, 0, 2)
-        assert "2 текстовых сообщения" in msg
+        assert "2 text messages" in msg
+
+    def test_texts_plural_five(self):
+        msg = build_stats_message(0, 0, 5)
+        assert "5 text messages" in msg
 
     def test_mixed_photos_and_texts(self):
         msg = build_stats_message(0, 2, 3)
-        assert "2 фото" in msg
-        assert "3 текстовых сообщения" in msg
+        assert "2 photos" in msg
+        assert "3 text messages" in msg
 
     def test_message_contains_pdf(self):
         msg = build_stats_message(0, 1, 1)
